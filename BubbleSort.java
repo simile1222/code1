365204b16d10705ac6d01eb45f2db2064132678b
Choi Dong-bin <43774570+DBChoi85@users.noreply.github.com>
2025-09-16 17:43:45 +0900

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,12 @@
+package com.example.strategy;
+
+public class BubbleSort implements SortStrategy {
+    @Override
+    public void sort(int[] numbers) {
+        // TODO: 버블 정렬 구현
+        // 힌트: 인접 원소를 비교해 큰 값을 뒤로 보내는 패스(outer)와
+        //       스왑을 수행하는 inner 루프를 작성하세요.
+        //       최적화: 스왑이 없으면 조기 종료.
+        throw new UnsupportedOperationException("TODO: BubbleSort.sort 구현");
+    }
+}
